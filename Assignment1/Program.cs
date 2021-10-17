59bcb300cb32c784f5a2254d08284441f0a5c148
Vishal Anal <vishalkumaranal@forcebolt.com>
2021-10-17 13:32:28 +0530

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            
+            Console.WriteLine("Input 10 No.:");
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n");
+            Array.Sort(arr);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Smallest No.:" + arr[0]+ "\n");
+            Console.WriteLine("Greatest No.:" + arr[arr.Length - 1]);
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+Assignment 1 : 
+- Create a console app 
+- Take 10 input from user 
+- Store input to array
+- Print the smallest & largest no of the array. 
+
+Working:-
+We create array 'arr' which will hold 10 values.
+we create loop for taking 10 inputs from user as intiger.
+We will sort the array using Array.Sort().
+We will print the smallest no. and greatest no. by calling 0th index and last index.
+
